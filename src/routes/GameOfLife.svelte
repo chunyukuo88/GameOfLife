baee8b170f840fa02202d9c553dc8c8896036df8
Alex Gochenour <alexgochenour@gmail.com>
2021-12-29 23:46:21 -0500

    Setup

---
+++ src/routes/GameOfLife.svelte
@@ -0,0 +1,22 @@
+<script lang='ts'>
+	const grid = [
+		[ 0, 0, 0, 0, 0 ],
+		[ 0, 0, 0, 0, 0 ],
+		[ 0, 0, 0, 0, 0 ],
+		[ 0, 0, 0, 0, 0 ],
+		[ 0, 0, 0, 0, 0 ],
+	];
+
+</script>
+
+<table>
+	{#each grid as row}
+		<tr>
+			{#each row as val}
+				<td>
+					{val}
+				</td>
+			{/each}
+		</tr>
+	{/each}
+</table>
