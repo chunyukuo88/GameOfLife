d0cfbf9c4605ac3b506934caa5cfa45fe9e52e3a
Alex Gochenour <alexgochenour@gmail.com>
2021-12-30 21:26:08 -0500

    Found the bug. Next: Refactoring.

--- src/lib/gameOfLifeUtils.spec.ts
+++ src/lib/gameOfLifeUtils.spec.ts
@@ -8,65 +8,67 @@ Any live cell with more than three live neighbours dies, as if by overpopulation
 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
  * */
 
-describe('cellClickHandler()', ()=>{
-	describe('GIVEN: A gridContext object, and row & value coordinates', ()=>{
-		describe('WHEN: All original grid values are negative', ()=>{
-			it('THEN: It updates the grid using the method in the gridContext.', ()=>{
-				let grid = [ // all negatives
-					[ -1, -1 ],
-					[ -1, -1 ],
-				];
-				const expectedResult = [
-					[ -1, -1 ],
-					[ -1,  1 ],
-				];
-				const gridContext = {
-					grid,
-					updateGrid: (newGrid) => grid = newGrid
-				};
+describe('gameOfLifeUtils.ts', ()=>{
+	describe('cellClickHandler()', ()=>{
+		describe('GIVEN: A gridContext object, and row & value coordinates', ()=>{
+			describe('WHEN: All original grid values are negative', ()=>{
+				it('THEN: It updates the grid using the method in the gridContext.', ()=>{
+					let grid = [ // all negatives
+						[ -1, -1 ],
+						[ -1, -1 ],
+					];
+					const expectedResult = [
+						[ -1, -1 ],
+						[ -1,  1 ],
+					];
+					const gridContext = {
+						grid,
+						updateGrid: (newGrid) => grid = newGrid
+					};
 
-				cellClickHandler(gridContext, 1, 1);
+					cellClickHandler(gridContext, 1, 1);
 
-				expect(grid).toEqual(expectedResult);
+					expect(grid).toEqual(expectedResult);
+				});
 			});
 		});
 	});
-});
-describe('evaluateAllCells()', ()=>{
-	describe('WHEN: The cell has only one neighbor', ()=>{
-		it('THEN: all cells die.', ()=>{
-			const grid = [
-				[ -1,  1,  1 ],
-				[ -1, -1, -1 ],
-				[ -1, -1, -1 ],
-			];
-			const expected = [
-				[ -1, -1, -1 ],
-				[ -1, -1, -1 ],
-				[ -1, -1, -1 ],
-			];
+	describe('evaluateAllCells()', ()=>{
+		describe('WHEN: The cell has only one neighbor', ()=>{
+			it('THEN: all cells die.', ()=>{
+				const grid = [
+					[ -1,  1,  1 ],
+					[ -1, -1, -1 ],
+					[ -1, -1, -1 ],
+				];
+				const expected = [
+					[ -1, -1, -1 ],
+					[ -1, -1, -1 ],
+					[ -1, -1, -1 ],
+				];
 
-			const result = evaluateAllCells(grid)
+				const result = evaluateAllCells(grid)
 
-			expect(result).toEqual(expected);
+				expect(result).toEqual(expected);
+			});
 		});
-	});
-	describe('WHEN: The 2 living cells have no neighbors', ()=>{
-		it('THEN: all cells die.', ()=>{
-			const grid = [
-				[ -1, -1,  1 ],
-				[ -1, -1, -1 ],
-				[  1, -1, -1 ],
-			];
-			const expected = [
-				[ -1, -1, -1 ],
-				[ -1, -1, -1 ],
-				[ -1, -1, -1 ],
-			];
+		describe('WHEN: The 2 living cells have no neighbors', ()=>{
+			it('THEN: all cells die.', ()=>{
+				const grid = [
+					[ -1, -1,  1 ],
+					[ -1, -1, -1 ],
+					[  1, -1, -1 ],
+				];
+				const expected = [
+					[ -1, -1, -1 ],
+					[ -1, -1, -1 ],
+					[ -1, -1, -1 ],
+				];
 
-			const result = evaluateAllCells(grid)
+				const result = evaluateAllCells(grid)
 
-			expect(result).toEqual(expected);
+				expect(result).toEqual(expected);
+			});
 		});
 	});
 });
--- src/lib/gameOfLifeUtils.ts
+++ src/lib/gameOfLifeUtils.ts
@@ -12,7 +12,7 @@ export const evaluateAllCells = (grid: Grid) => {
 	for (let i = 0; i < newGrid.length; i++){
 		if (i === 0) {
 			evaluateTopRow(newGrid, newGrid[i], i);
-		} else if (i > 0 && i < newGrid.length) {
+		} else if (i > 0 && i < newGrid.length - 1) {
 			evaluateMiddleRows(newGrid, newGrid[i], i);
 		} else {
 			evaluateBottomRow(newGrid, newGrid[i], i);
@@ -98,38 +98,57 @@ const getNeighborsForBottomMiddleVals = (newGrid: Grid, i, j) => {
 const getNeighborsForLowerLeftCorner = (newGrid: Grid, i, j) => [newGrid[i-1][j], newGrid[i-1][j+1], newGrid[i][j+1]];
 const getNeighborsForLowerRightCorner = (newGrid: Grid, i, j) => [newGrid[i-1][j-1], newGrid[i-1][j], newGrid[i][j-1]];
 
-const evaluateMiddleRows = (newGrid: Grid, row, i) => {
+const evaluateMiddleRows = (newGrid: Grid, row, i):void => {
 	for (let j = 0; j < row.length - 1; j++){
 		let currentCell = newGrid[i][j];
 		const totalLivingNeighbors = getTotalLivingNeighbors(newGrid, i, j);
-		if (totalLivingNeighbors > 2) {
+		if (totalLivingNeighbors > 3 || totalLivingNeighbors < 2) {
 			currentCell = -1;
 		}
 	}
 };
 
-const getTotalLivingNeighbors = (newGrid: Grid, i, j): number => {
+const getTotalLivingNeighbors = (newGrid: Grid, i, j):number => {
 	let totalLivingNeighbors = 0;
-	const nwNeighbor = newGrid[i - 1][j - 1];
-	const northNeighbor = newGrid[i - 1][j];
-	const neNeighbor = newGrid[i - 1][j + 1];
-	const eastNeighbor = newGrid[i][j - 1 ];
-	const westNeighbor = newGrid[i][j + 1 ];
-	const swNeighbor = newGrid[i + 1][j - 1];
-	const southNeighbor = newGrid[i + 1][j];
-	const seNeighbor = newGrid[i + 1][j + 1];
-
-	if (nwNeighbor !== undefined && nwNeighbor === 1) totalLivingNeighbors++;
-	if (northNeighbor && northNeighbor === 1) totalLivingNeighbors++;
-	if (neNeighbor && neNeighbor === 1) totalLivingNeighbors++;
-
-
-	if (eastNeighbor && eastNeighbor === 1) totalLivingNeighbors++;
-	if (westNeighbor && westNeighbor === 1) totalLivingNeighbors++;
-
-
-	if (swNeighbor && swNeighbor === 1) totalLivingNeighbors++;
-	if (southNeighbor && southNeighbor === 1) totalLivingNeighbors++;
-	if (seNeighbor && seNeighbor === 1) totalLivingNeighbors++;
+	if (j === 0) {
+		const eastNeighbor = newGrid[i][j + 1 ];
+		const neNeighbor = newGrid[i - 1][j + 1];
+		const northNeighbor = newGrid[i - 1][j];
+		const seNeighbor = newGrid[i + 1][j + 1];
+		const southNeighbor = newGrid[i + 1][j];
+		if (eastNeighbor  === 1) totalLivingNeighbors++;
+		if (neNeighbor    === 1) totalLivingNeighbors++;
+		if (northNeighbor === 1) totalLivingNeighbors++;
+		if (seNeighbor    === 1) totalLivingNeighbors++;
+		if (southNeighbor === 1) totalLivingNeighbors++;
+	} else if (i > 0 && i < newGrid.length - 1) {
+		const eastNeighbor = newGrid[i][j + 1 ];
+		const westNeighbor = newGrid[i][j - 1 ];
+		const nwNeighbor = newGrid[i - 1][j - 1];
+		const northNeighbor = newGrid[i - 1][j];
+		const neNeighbor = newGrid[i - 1][j + 1];
+		const swNeighbor = newGrid[i + 1][j - 1];
+		const southNeighbor = newGrid[i + 1][j];
+		const seNeighbor = newGrid[i + 1][j + 1];
+		if (eastNeighbor === 1) totalLivingNeighbors++;
+		if (westNeighbor === 1) totalLivingNeighbors++;
+		if (nwNeighbor === 1) totalLivingNeighbors++;
+		if (northNeighbor === 1) totalLivingNeighbors++;
+		if (neNeighbor === 1) totalLivingNeighbors++;
+		if (swNeighbor === 1) totalLivingNeighbors++;
+		if (southNeighbor === 1) totalLivingNeighbors++;
+		if (seNeighbor === 1) totalLivingNeighbors++;
+	} else {
+		const westNeighbor = newGrid[i][j - 1 ];
+		const nwNeighbor = newGrid[i - 1][j - 1];
+		const northNeighbor = newGrid[i - 1][j];
+		const swNeighbor = newGrid[i + 1][j - 1];
+		const southNeighbor = newGrid[i + 1][j];
+		if (westNeighbor === 1) totalLivingNeighbors++;
+		if (nwNeighbor === 1) totalLivingNeighbors++;
+		if (northNeighbor === 1) totalLivingNeighbors++;
+		if (swNeighbor === 1) totalLivingNeighbors++;
+		if (southNeighbor === 1) totalLivingNeighbors++;
+	}
 	return totalLivingNeighbors;
 };
