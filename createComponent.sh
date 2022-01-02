bf33c1d9e6492ab60d50f5f0a2b0bb8c64a6f469
chunyukuo88 <alexgochenour@gmail.com>
2022-01-02 15:58:28 -0500

    Done

--- createComponent.sh
+++ createComponent.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "+-------------------------+"
-echo "|   Create a component:   |"
-echo "+-------------------------+"
+echo "+-------------------------------------------------------------------+"
+echo "|   Create a component, its utils file, and test suites for both:   |"
+echo "+-------------------------------------------------------------------+"
 
 read -p "Name your new component:" componentName
 read -p "Confirm you want to create a component called $componentName [y/n]:" create
@@ -12,11 +12,11 @@ if [ "$create" == "y" ]
     cd src/lib/"$componentName"
     cat ../Template/Template.svelte > "$componentName".svelte
     cat ../Template/Template.spec.ts > "$componentName".spec.ts
+    cat ../Template/TemplateUtils.ts > "$componentName"Utils.ts
+    cat ../Template/TemplateUtils.spec.ts > "$componentName"Utils.spec.ts
     for FILE in *
     do
        sed -i "s/Template/$componentName/g" $FILE
     done
-#    cat ../TemplateUtils.ts > "$componentNameUtils".ts
-#    cat ../TemplateUtils.spec.ts > "$componentNameUtils".spec.ts
   else exit 1
 fi
