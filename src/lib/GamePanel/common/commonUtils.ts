4737ea85b890a05aba957ddee6dc7e0ba8620db6
Alex Gochenour <alexgochenour@gmail.com>
2022-01-22 00:57:50 -0500

    WIP: I think the problem is in how the OnOffSwitch does not yet use the ticking context.

--- src/lib/GamePanel/common/commonUtils.ts
+++ src/lib/GamePanel/common/commonUtils.ts
@@ -10,15 +10,16 @@ const speedStore = createSpeedStore();
 
 export const createGridContext = () => ({
 	gridStore,
-	updateGrid: (newGrid) => gridStore.set(newGrid),
+	updateGrid: (newGrid: number[][]) => gridStore.set(newGrid),
 });
 
 export const createTickingContext = () => ({
 	isTickingStore,
 	stopTicking: () => isTickingStore.set(false),
+	startTicking: () => isTickingStore.set(false),
 });
 
 export const createSpeedContext = () => ({
 	speedStore,
-	updateSpeed: (newSpeed) => speedStore.set(newSpeed),
+	updateSpeed: (newSpeed: number) => speedStore.set(newSpeed),
 });
